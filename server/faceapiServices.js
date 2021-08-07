bf40a6928a416c27e2773a49257701ce787c1c66
Phong Duy Bui <buiduyphong921@gmail.com>
2021-08-07 15:18:46 +0700

    Implement recognition face server

--- server/faceapiServices.js
+++ server/faceapiServices.js
@@ -1,10 +1,12 @@
-import * as canvas from 'canvas';
+import canvas from 'canvas';
 import * as faceapi from 'face-api.js';
 import * as tf from '@tensorflow/tfjs-node';
 import fetch from 'node-fetch';
 import path from 'path';
+import colors from 'colors';
+import db from './db.js';
 
-const { Canvas, Image, ImageData } = canvas;
+const { Canvas, Image, ImageData, loadImage } = canvas;
 faceapi.env.monkeyPatch({
   Canvas,
   Image,
@@ -12,33 +14,85 @@ faceapi.env.monkeyPatch({
   fetch,
 });
 
-async function initTensorflow() {
-  await faceapi.tf.setBackend('tensorflow');
-  await faceapi.tf.enableProdMode();
-  await faceapi.tf.ENV.set('DEBUG', false);
-  await faceapi.tf.ready();
-}
-initTensorflow();
+Promise.all([
+  await faceapi.tf.setBackend('tensorflow'),
+  await faceapi.tf.enableProdMode(),
+  await faceapi.tf.ENV.set('DEBUG', false),
+  await faceapi.tf.ready(),
+]).then(() => console.log('Set backend successfull!'.red.bgBlue));
 
 const __dirname = path.resolve();
 const MODELS_URL = path.join(__dirname, 'server/public/models');
 
-async function init() {
-  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODELS_URL);
-  await faceapi.nets.faceRecognitionNet.loadFromDisk(MODELS_URL);
-}
-init();
+Promise.all([
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODELS_URL),
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(MODELS_URL),
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(MODELS_URL),
+]).then(() => console.log('Models loaded!'.red.bgYellow));
 
 export async function decodeImage(file) {
-  const decoded = tf.node.decodeImage(file);
-  const casted = decoded.toFloat();
-  const result = casted.expandDims(0);
-  decoded.dispose();
-  casted.dispose();
-  return result;
+  // const decoded = tf.node.decodeImage(file);
+  // const casted = decoded.toFloat();
+  // const result = casted.expandDims(0);
+  // decoded.dispose();
+  // casted.dispose();
+  // return result;
+  const img = await loadImage(file);
+  return img;
 }
 
 export async function detectAllFace(decodedImage) {
-  const result = await faceapi.detectAllFaces(decodedImage);
+  const result = await faceapi
+    .detectAllFaces(decodedImage)
+    .withFaceLandmarks()
+    .withFaceDescriptors();
   return result;
+}
+
+function loadDescriptors() {
+  const labeledDescriptors = db.map(async (user) => {
+    const imgUrl = `http://localhost:5000/${user.img}`;
+    const imgSource = await loadImage(imgUrl);
+    const { descriptor } = await faceapi
+      .detectSingleFace(imgSource)
+      .withFaceLandmarks()
+      .withFaceDescriptor();
+
+    const labeledDescriptor = new faceapi.LabeledFaceDescriptors(user.label, [
+      descriptor,
+    ]);
+    return labeledDescriptor;
+  });
+
+  return Promise.all(labeledDescriptors);
+}
+
+export async function detectSingleFace(decodedImage) {
+  const labeledDescriptors = await loadDescriptors();
+  const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+  console.log(faceMatcher);
+
+  const result = await faceapi
+    .detectSingleFace(decodedImage)
+    .withFaceLandmarks()
+    .withFaceDescriptor();
+
+  const out = faceapi.createCanvasFromMedia(decodedImage);
+
+  // faceapi.draw.drawDetections(out, result);
+
+  // console.log(out.toDataURL());
+
+  const bestMatch = faceMatcher.findBestMatch(result.descriptor);
+  if (bestMatch.distance < 0.5) {
+    console.log('Diem danh thanh cong');
+  }
+
+  const box = result.detection.box;
+  const drawBox = new faceapi.draw.DrawBox(box, {
+    label: `${bestMatch.label} (${Number(1 - bestMatch.distance).toFixed(2)})`,
+  });
+  drawBox.draw(out);
+
+  return out.toDataURL();
 }
